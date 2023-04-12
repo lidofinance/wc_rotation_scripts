053f916eaa11d150e63bbf915a025d0ee4be2eb1
George Avsetsin <mail@sjors.ru>
2023-04-12 21:46:24 +0400

    feat: run checks in loop

--- scripts/fetch-rotation-pool.ts
+++ scripts/fetch-rotation-pool.ts
@@ -1,9 +1,19 @@
 import { Argument, Command, Option } from 'commander';
 import * as dotenv from 'dotenv';
 import { fetchBLSToExecutionChanges, readValidatorsIndexesFile } from './shared';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
 dotenv.config();
 const program = new Command();
+
+const playAlarm = async () => {
+  const execPromise = promisify(exec);
+
+  while (true) {
+    await execPromise('afplay /System/Library/Sounds/Ping.aiff');
+  }
+};
 
 program
   .addOption(
@@ -11,8 +21,9 @@ program
       .env('CONSENSUS_LAYER')
       .makeOptionMandatory(),
   )
+  .addOption(new Option('-e, --execution-address <string>', 'Execution address').default(''))
   .addArgument(new Argument('<file-path>', 'File to filter validators'))
-  .action(async (filePath, { consensusLayer }) => {
+  .action(async (filePath, { consensusLayer, executionAddress }) => {
     /**
      * Read the validator indexes file content
      * Indexes must be separated by `\n`
@@ -21,22 +32,43 @@ program
     const validatorIndexes = readValidatorsIndexesFile(filePath);
     console.log('Validator indexes are read:', validatorIndexes.length);
 
-    /**
-     * Collect messages to rotate withdrawals credentials from pool
-     */
-    console.log('Fetching rotation message from Consensus Layer...');
-    const messagesInPool = await fetchBLSToExecutionChanges(consensusLayer);
-    console.log('Messages fetched');
+    do {
+      /**
+       * Collect messages to rotate withdrawals credentials from pool
+       */
+      console.log('Fetching rotation message from Consensus Layer...');
+      const messagesInPool = await fetchBLSToExecutionChanges(consensusLayer);
+      console.log('Messages fetched');
 
-    /**
-     * Collect statistics
-     */
-    const filteredSet = new Set(validatorIndexes);
-    const messagesFromFile = messagesInPool.filter(({ message }) => filteredSet.has(Number(message.validator_index)));
+      /**
+       * Collect statistics
+       */
+      const filteredSet = new Set(validatorIndexes);
+      const messagesFromFile = messagesInPool.filter(({ message }) => filteredSet.has(Number(message.validator_index)));
+      const unknownAddress = messagesFromFile.filter(({ message }) => message.to_execution_address != executionAddress);
 
-    console.table({
-      'Total messages in pool': messagesInPool.length,
-      'Messages from validators from the file': messagesFromFile.length,
-    });
+      console.table({
+        'Total messages in pool': messagesInPool.length,
+        'Messages from validators from the file': messagesFromFile.length,
+        'Unknown execution address for known validators': unknownAddress.length,
+      });
+
+      if (unknownAddress.length) {
+        console.warn('Found unknown withdrawal credentials');
+        console.table(
+          unknownAddress.map(({ message: { validator_index, to_execution_address } }) => ({
+            validator_index,
+            to_execution_address,
+          })),
+        );
+
+        await playAlarm();
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 300_000));
+      console.log();
+      console.log('----------------------------------------');
+      console.log();
+    } while (true);
   })
   .parse(process.argv);
